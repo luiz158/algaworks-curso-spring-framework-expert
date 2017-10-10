b46c63dc9db48410339010ec01eae716c770fe62
Alexandre Afonso <afonsoaaf@gmail.com>
2017-10-10 18:39:20 -0300

    Adaptando o projeto do curso para utilizar Spring Framework 5.

---
+++ src/main/java/com/algaworks/brewer/controller/RelatoriosController.java
@@ -0,0 +1,75 @@
+package com.algaworks.brewer.controller;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.brewer.dto.PeriodoRelatorio;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+@Controller
+@RequestMapping("/relatorios")
+public class RelatoriosController {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@GetMapping("/vendasEmitidas")
+	public ModelAndView relatorioVendasEmitidas() {
+		ModelAndView mv = new ModelAndView("relatorio/RelatorioVendasEmitidas");
+		mv.addObject(new PeriodoRelatorio());
+		return mv;
+	}
+	
+	@ResponseBody
+	@PostMapping("/vendasEmitidas")
+	public ResponseEntity<byte[]> gerarRelatorioVendasEmitidas(PeriodoRelatorio periodoRelatorio) throws Exception {
+		Date dataInicio = Date.from(LocalDateTime.of(periodoRelatorio.getDataInicio(), LocalTime.of(0, 0, 0))
+				.atZone(ZoneId.systemDefault()).toInstant());
+		Date dataFim = Date.from(LocalDateTime.of(periodoRelatorio.getDataFim(), LocalTime.of(23, 59, 59))
+				.atZone(ZoneId.systemDefault()).toInstant());
+		
+		Map<String, Object> parametros = new HashMap<>();
+		parametros.put("format", "pdf");
+		parametros.put("data_inicio", dataInicio);
+		parametros.put("data_fim", dataFim);
+		
+		InputStream inputStream = this.getClass().getResourceAsStream("/relatorios/relatorio_vendas_emitidas.jasper");
+		
+		JasperPrint jasperPrint = null;
+		Connection con = this.dataSource.getConnection();
+		
+		try {
+			jasperPrint = JasperFillManager.fillReport(inputStream, parametros, con);			
+		}
+		finally {
+			con.close();
+		}
+		
+		byte[] retorno = JasperExportManager.exportReportToPdf(jasperPrint);
+		
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_TYPE, "application/pdf")
+				.body(retorno);
+	}
+}
